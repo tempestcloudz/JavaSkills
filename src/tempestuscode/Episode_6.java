b7bead2e8988e1e90e96ed404fbcf4688528adaa
cloudz_code <hatyco59@gmail.com>
2025-09-01 22:33:48 +0800

    refactoring...
    Signed-off-by: cloudz_code <hatyco59@gmail.com>

--- src/tempestuscode/Episode_6.java
+++ src/tempestuscode/Episode_6.java
@@ -73,4 +73,27 @@ public class Episode_6 {
 
 
     }
+
+    public static class tests_Episode_6 {
+        public static void main(String [] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Are you going to draw a card?");
+            boolean isDrawing = scanner.nextBoolean();
+
+            System.out.println("Do you have a library?");
+            boolean hasLibrary = scanner.nextBoolean();
+
+            boolean canDraw = isDrawing && hasLibrary;
+            System.out.println("Do you draw? " + canDraw);
+
+            System.out.println("Give a number...");
+            int number = scanner.nextInt();
+
+            boolean numberVs100 = number >= 100;
+            System.out.println("Is your number greater than 100? " + numberVs100);
+
+        }
+
+    }
 }
